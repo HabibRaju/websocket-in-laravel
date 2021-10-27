b16e27bdcff560d7c7165387f6700700b5cea245
HabiburRaju <habibur.rahaman@brainstation-23.com>
2021-10-27 17:36:42 +0600

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 25)->create()->each(function ($u) {
+            for ($i=0; $i <= 3; $i++) {
+              $u->posts()->save(factory(App\Post::class)->make());
+            }  
+          });
+    }
+}
